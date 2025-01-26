7ae07e028ca3b2f3d64ae05b8afe75110e581b25
Mansuri Villa <mansuriadnan2004@gmail.com>
2025-01-26 17:13:30 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Products from "./components/Products"; // Ensure this path is correct
-import ProductPage from "./components/ProductPage"; // Ensure this path is correct
 
 const App = () => {
   return (
