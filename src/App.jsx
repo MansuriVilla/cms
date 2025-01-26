51c0783d5be18876ebe7bee49a1840b3c4323002
Mansuri Villa <mansuriadnan2004@gmail.com>
2025-01-26 17:25:34 +0530

    Fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Products from "./components/products";
-import ProductPage from "./components/productPage";
+import productPage from "./components/productPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Products />} />
-        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/product/:id" element={<productPage />} />
       </Routes>
     </Router>
   );
