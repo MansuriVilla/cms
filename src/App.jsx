b93eaeccf42dc740d2e3ac083265b5eeff5dbed0
Mansuri Villa <mansuriadnan2004@gmail.com>
2025-01-26 17:33:55 +0530

    Changed The File

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Products from "./components/products";
-import ProductPage from "./components/productPage";
+import ProductPage from "./components/ProductPages";
 
 const App = () => {
   return (
